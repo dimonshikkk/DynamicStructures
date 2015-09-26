9e77e0a54e884001e8244b7046597ea5cf96bc85
dimonshikkk <dima3456737@mail.ru>
2015-09-26 13:31:09 +0300

    Create queue.h

---
+++ queue.h
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+struct node
+{
+	int d;
+	node *p;
+};
+
+node *first(int d);
+void add(node **pend, int d);
+int del(node **pbeg);
