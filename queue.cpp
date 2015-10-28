ba6ef0b43196bd77b24333bd2f4ab5d5dcb924ad
dimonshikkk <dima3456737@mail.ru>
2015-10-28 21:38:57 +0300

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -2,6 +2,13 @@
 #include "queue.h"
 #include <iostream>
 
+
+
+/*
+	BRIEF: Функция вставки первого элемента в очередь
+	IN: Число,которое будет элементом в очереди
+	OUT: Адрес первого элемента структуры
+*/
 node *first(int d)
 {
 	node *pv = new node;
@@ -11,7 +18,11 @@ node *first(int d)
 }
 
 
-
+/*
+	BRIEF: Функция добавление элемента в очередь
+	IN: Адрес вершины,число,котоое вставят в очередь
+	OUT: ---
+*/
 void add(node **pend, int d)
 {
 	node *pv = new node;
@@ -23,17 +34,25 @@ void add(node **pend, int d)
 
 
 
-int del(node **pbeg)
+/*
+	BRIEF: Функция удаления элемента из очереди
+	IN: Адрес вершины
+	OUT: ---
+*/
+void del(node **pbeg)
 {
-	int temp = (*pbeg)->d;
 	node *pv = *pbeg;
 	*pbeg = (*pbeg)->p;
 	delete pv;
-	return temp;
 }
 
 
 
+/*
+	BRIEF: Функция печати очереди
+	IN: Адрес вершины
+	OUT: Элементы очереди на экран
+*/
 void print(node *pbeg)
 {
 	node *pv = pbeg;
@@ -47,12 +66,13 @@ void print(node *pbeg)
 
 
 
+/*
+	BRIEF: Функция проверку на пустоту
+	IN: Адрес вершины 
+	OUT: 0,если элементов в очереди нет,1 в ином случае
+*/
 bool isEmpty(node *pbeg)
 {
-	//node *pv = pbeg;
-	//if (pv == NULL) return 0;
-	//else return 1;
-
 	if (pbeg == NULL)
 	{
 		return 0;
